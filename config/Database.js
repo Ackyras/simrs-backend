44c224e68b537898c827753869c595fe880543ab
ackyras <ackyrasibarani@gmail.com>
2024-07-20 16:18:20 +0700

    update db config

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 const db = new Sequelize(
     process.env.DB_NAME,
@@ -6,7 +6,7 @@ const db = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB,
+        dialect: 'mysql',
     }
 );
 
