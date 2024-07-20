d9b981d6d8ed82d57707aa21b5caea5467c05913
ackyras <ackyrasibarani@gmail.com>
2024-07-20 14:58:00 +0700

    update db setting

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("simrs_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
+const db = new Sequelize('sql12721052', 'sql12721052', 'XdYG4NWsfJ', {
+    host: 'sql12.freesqldatabase.com',
+    dialect: 'mysql',
 });
 
 export default db;
